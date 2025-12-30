6f7f742407f9642844a446212202ff8d2912d428
Eric Chen <eric.chen682@gmail.com>
2025-12-30 13:03:17 -0800

    Create and display initial 16x16 grid

---
+++ script.js
@@ -0,0 +1,15 @@
+// initial grid generation
+
+const grid = document.querySelector("#grid");
+for(let i = 0; i < 16; i++)
+{
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for(let j = 0; j < 16; j++)
+    {
+        const col = document.createElement("div");
+        col.classList.add("box");
+        row.appendChild(col);
+    }
+    grid.appendChild(row);
+}
