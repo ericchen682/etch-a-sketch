581bec58e139d3774b95a30563592c7b47bc4e08
Eric Chen <eric.chen682@gmail.com>
2025-12-30 23:12:50 -0800

    Added customizable grid sizing

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // initial grid generation
 
 const grid = document.querySelector("#grid");
+const btn = document.querySelector("button");
+
 for(let i = 0; i < 16; i++)
 {
     const row = document.createElement("div");
@@ -16,3 +18,33 @@ for(let i = 0; i < 16; i++)
     }
     grid.appendChild(row);
 }
+
+btn.addEventListener("click", () => {
+    let width = prompt("Please provide a value between 1 and 100 as the number of squares per side of the grid.");
+    while(Number(width) === NaN || Number(width) <= 0 || Number(width) > 100)
+    {
+        width = prompt("Please provide a valid value.");
+    }
+    while(grid.firstChild)
+    {
+        grid.removeChild(grid.lastChild);
+    }
+    for(let i = 0; i < width; i++)
+{
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for(let j = 0; j < width; j++)
+    {
+        const col = document.createElement("div");
+        col.classList.add("box");
+        col.style.width = 32/width + "vw";
+        col.style.width /= width;
+        col.style.height = col.style.width;
+        row.appendChild(col);
+        col.addEventListener("mouseenter", () => {
+            col.style.backgroundColor = "gray";
+        })
+    }
+    grid.appendChild(row);
+}
+})
